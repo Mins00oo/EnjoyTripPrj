c08b9ab7f415009356a4c428bc86ef8b822252a8
RaelJung <allice2283006@gmail.com>
2023-11-20 10:02:05 +0900

    conflict 해결

--- src/main/java/com/mycom/joytrip/config/TestInterceptor.java
+++ src/main/java/com/mycom/joytrip/config/TestInterceptor.java
@@ -17,24 +17,6 @@ public class TestInterceptor implements HandlerInterceptor{
 		  
 		System.out.println("TestInterceptor >>> " + request.getRequestURI());
 		
-<<<<<<< HEAD
-	//		HttpSession session = request.getSession();
-	//		UserDto userDto = (UserDto) session.getAttribute("userDto");
-	//		
-	//		System.out.println(request.getMethod());
-	//		
-	//		if (userDto == null) {
-	//			if (request.getMethod().equals("POST") || request.getMethod().equals("OPTIONS")) {
-	//				
-	//				System.out.println("NO INterceptor");
-	//				return true;
-	//			}
-	//			// 로그인 필요함
-	//			response.getWriter().write(jsonStr);
-	//			System.out.println("need login");
-	//			return false;
-	//		}
-=======
 		HttpSession session = request.getSession();
 		UserDto userDto = (UserDto) session.getAttribute("userDto");
 		
@@ -51,7 +33,6 @@ public class TestInterceptor implements HandlerInterceptor{
 			System.out.println("need login");
 			return false;
 		}
->>>>>>> 68d37fcfd54314b4826f1cb6339a816c9bb191ea
 		
 		return true;
 		
