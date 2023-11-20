ab3dd37da934e7704f3850102f953e13c9ba7318
mins00oo <alstn7223@gmail.com>
2023-11-20 17:42:50 +0900

    관광지 즐겨찾기 등록

--- src/main/java/com/mycom/joytrip/star/controller/StarController.java
+++ src/main/java/com/mycom/joytrip/star/controller/StarController.java
@@ -28,8 +28,9 @@ public class StarController {
 	
 	@PostMapping("/tours/stars")
 	public ResponseEntity<Object> registTourStar(@RequestBody StarRequestDto starDto, HttpSession session) {
-//		UserDto userDto = (UserDto) session.getAttribute("userDto");
-		starDto.setUserId(1);
+		System.out.println(starDto);
+		UserDto userDto = (UserDto) session.getAttribute("userDto");
+		starDto.setUserId(userDto.getUserId());
 		starService.registStar(starDto);
 		
 		return ResponseEntity.status(200).body("즐겨찾기 등록이 완료되었습니다");
